68153eda7baf97a556bf13d03688bb224d5daaef
agesquivel <agesquivel@gmail.com>
2018-02-08 11:08:07 -0500

    Venta a básica con Label

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.Color;
+import java.awt.Container;
 
 public class Main {
-
 
     public static void main(String[] args)
     {
@@ -10,8 +10,27 @@ public class Main {
         f.setSize(300,300);
         f.setLocation(200,100);
         f.setBackground(Color.cyan);
+
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.getContentPane().setBackground(Color.GREEN);
+        f.setResizable(false);
+        Container panelPrincipal = f.getContentPane();
+
+        panelPrincipal.setBackground(Color.GREEN);
+
+        panelPrincipal.setLayout(null);
+        JLabel label1=new JLabel("Hola Mundo.");
+        label1.setBounds(10,20,200,30);
+        panelPrincipal.add(label1);
+
         f.setVisible(true);
     }
 }
+
+
+
+
+
+
+
+
+
