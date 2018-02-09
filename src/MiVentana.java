7e3e117c08bc6cf32eb1d92726018b5200c14988
agesquivel <agesquivel@gmail.com>
2018-02-09 11:58:53 -0500

    Ventana con validación, cierre y uso de JPasswordField

--- src/MiVentana.java
+++ src/MiVentana.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 public class MiVentana extends JFrame{
 
     private JButton btnAceptar;
+    private JTextField edtUsr;
+    private JPasswordField edtClave;
 
     public MiVentana(String titulo, Color colorfondo) {
         this.setTitle(titulo);
@@ -17,14 +19,14 @@ public class MiVentana extends JFrame{
         JLabel lblUsr = new JLabel("Usuario: ");
         lblUsr.setBounds(10,20,100,30);
         panelPrincipal.add(lblUsr);
-        JTextField edtUsr = new JTextField();
+        edtUsr = new JTextField();
         edtUsr.setBounds(120, 20, 150, 30);
         panelPrincipal.add(edtUsr);
 
         JLabel lblClave = new JLabel("Contraseña: ");
         lblClave.setBounds(10,60,100,30);
         panelPrincipal.add(lblClave);
-        JTextField edtClave = new JTextField();
+        edtClave = new JPasswordField();
         edtClave.setBounds(120, 60, 150, 30);
         panelPrincipal.add(edtClave);
 
@@ -53,6 +55,7 @@ public class MiVentana extends JFrame{
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setResizable(false);
         f.setVisible(true);
+
     }
 
     //Declaración de clase interna
@@ -61,12 +64,20 @@ public class MiVentana extends JFrame{
         //Método controlador del evento ActionEvent
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("Haz presionado el botón "
+            /*System.out.println("Haz presionado el botón "
                     + ((JButton) e.getSource()).getText() );
+            */
 
-            if ((JButton) e.getSource() == btnAceptar)
-                System.out.println("Presionaste el botón Aceptar");
-            else System.out.println("Presionaste el botón Cancelar");
+            if ((JButton) e.getSource() == btnAceptar) {
+                if (edtUsr.getText().contentEquals("admin")
+                        && edtClave.getText().contentEquals("1234"))
+                    System.out.println("Acceso concedido");
+                else System.out.println("Acceso denegado. Intente de nuevo");
+            }
+            else {
+                //this.dispose();
+                System.exit(0); //Cerrar toda la aplicación
+            }
         }
     } //Class AccionBoton
 } //Class MiVentana
